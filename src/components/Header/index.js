15b553f9cda1575aceb8d1e1eefeeac6ebb5e2e5
DamienCourcoux <damsmailspro@gmail.com>
2021-10-18 10:52:20 +0200

    menu burger

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,22 @@
 // == Import
-
+import { slide as Menu } from 'react-burger-menu';
 import Logo from 'src/assets/img/logo.png';
-import { FaBars } from 'react-icons/fa';
 import './header.scss';
+
+// const showSettings = (event) => {
+//   event.preventDefault();
+// };
 
 // == Composant
 const Header = () => (
   <header className="header">
     <a href="/" title="Damien Courcoux"><img className="header__logo" src={Logo} alt="logo du site portfolio" /></a>
-    <span className="header__menu" title="Menu"><FaBars /></span>
+    <Menu right width="100%" title="Menu">
+      <a href="/" title="Damien Courcoux"><img className="header__logo" src={Logo} alt="logo du site portfolio" /></a>
+      <a id="home" className="menu-item" href="/" title="Accueil">Accueil</a>
+      <a id="project" className="menu-item" href="/about" title="Mes projets">Mes projets</a>
+      <a id="cv" className="menu-item" href="/contact" title="Mon CV">Mon CV</a>
+    </Menu>
   </header>
 );
 
